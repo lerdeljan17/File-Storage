d913df82dfd52c61bc67d54019081448ce82d5aa
lerdeljan17 <lerdeljan17@raf.rs>
2019-10-21 11:27:28 +0200

    dir promene

--- FIleStorageSpi/src/main/java/raf/rs/FIleStorageSpi/myDir.java
+++ FIleStorageSpi/src/main/java/raf/rs/FIleStorageSpi/myDir.java
@@ -7,4 +7,10 @@ public interface myDir {
 	boolean initFileStorage(StorageUser creator, String path, String rootDirName);
 
 	File searchDirectory(String dirPath, String searchFor);
+	
+	boolean createMultipleDirectories(String dirsName,int numberOfDirs);
+	
+	boolean createEmptyDir(String path, String fileName);
+	
+	
 }
