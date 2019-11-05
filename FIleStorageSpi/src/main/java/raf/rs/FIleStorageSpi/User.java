27a482a364dbd6fbed1124108690a3383f5e2662
lerdeljan17 <lerdeljan17@raf.rs>
2019-11-05 23:32:13 +0100

    d

--- FIleStorageSpi/src/main/java/raf/rs/FIleStorageSpi/User.java
+++ FIleStorageSpi/src/main/java/raf/rs/FIleStorageSpi/User.java
@@ -8,62 +8,87 @@ public class User {
 	private String password;
 	private ArrayList<String> privilages;
 	private boolean isRootUser;
+
 	public User(String username, String password, ArrayList<String> privilages) {
 		super();
 		this.username = username;
 		this.password = password;
 		this.privilages = privilages;
 	}
-	public  boolean createNewUser(String username, String password) {
-		return isRootUser;};
-	
-	public  void revokePrivilage(User revokeFromUser,String privilage) {
+
+	public User(String username, String password) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.privilages = new ArrayList<String>();
+		this.privilages.add("add");
+		this.privilages.add("del");
+		this.privilages.add("read");
 	}
-	
-	public  boolean connectToFileStorage(String rootDirPath,String FileStorageRootDirName)throws Exception {
+
+	public boolean createNewUser(String username, String password) {
+
+		return true;
+	}
+
+	public void revokePrivilage(User revokeFromUser, String privilage) {
+		
+	}
+
+	public boolean connectToFileStorage(String rootDirPath, String FileStorageRootDirName) throws Exception {
 		return false;
 	}
-	
+
 	public boolean disconnectFromFileStorage(String fileStorageRootDir) {
 		return false;
 	}
+
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public ArrayList<String> getPrivilages() {
 		return privilages;
 	}
+
 	public void setPrivilages(ArrayList<String> privilages) {
 		this.privilages = privilages;
 	}
+
 	public boolean isRootUser() {
 		return isRootUser;
 	}
+
 	public void setRootUser(boolean isRootUser) {
 		this.isRootUser = isRootUser;
 	}
+
 	@Override
 	public String toString() {
 		// TODO Auto-generated method stub
-		return this.username + " " + this.password +" " +this.privilages.toString();
+		return this.username + " " + this.password + " " + this.privilages.toString();
 	}
-	
+
 	@Override
 	public boolean equals(Object obj) {
 		// TODO Auto-generated method stub
-		if(obj instanceof User) {
+		if (obj instanceof User) {
 			User tmpUser = (User) obj;
-			if(this.username.equals(tmpUser.getUsername()))return true;
-		}else {
+			if (this.username.equals(tmpUser.getUsername()))
+				return true;
+		} else {
 			return false;
 		}
 		return super.equals(obj);
